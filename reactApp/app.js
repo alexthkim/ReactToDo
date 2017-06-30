a996238a2778abc7c5ae51fc2eb0d532508461ee
alexthkim <atk3@rice.edu>
2017-06-30 13:57:00 -0700

    basic outline of the todo list

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>, document.getElementById('root'));
+var dummyData = ["Do the dishes", "Read a book", "Eat pasta"];
+
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <InputLine/>
+        <TodoList/>
+      </div>
+    )
+  }
+}
+
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <input type="text" placeholder="To Do Item"></input>
+        <input type="submit" value="Submit"></input>
+      </div>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <ul>
+        {dummyData.map((item) => <Todo task={item}/>)}
+      </ul>
+    )
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <li>
+        {this.props.task}
+        <button>X</button>
+      </li>
+
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
